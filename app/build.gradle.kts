45a7d184fa7898e69a3f006b59540006813ba282
Dmitry Gorbunov <dimarik70rus@gmail.com>
2025-07-29 09:53:40 +0700

    project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
