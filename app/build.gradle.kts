d8469fadda081445ffac957d6f6a5de26bfcb089
Dmitry Gorbunov <dimarik70rus@gmail.com>
2025-07-29 16:46:17 +0700

    picocli setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     application
+    kotlin("jvm") version "2.2.0"
+    kotlin("kapt") version "2.2.0"
 }
 
 group = "hexlet.code"
@@ -10,12 +12,15 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.commons:commons-lang3:3.18.0")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.7")
+    kapt("info.picocli:picocli-codegen:4.7.7")
 }
+
 
 tasks.test {
     useJUnitPlatform()
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+@Command(
+        name="gendiff",
+        mixinStandardHelpOptions = true,
+        description="Compares two configuration files and shows a difference.")
+
+public class App implements Runnable{
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Запущено приложение");
+        System.out.println("Verbose: " );
     }
 }
